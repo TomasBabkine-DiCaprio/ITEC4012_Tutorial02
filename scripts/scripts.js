c30b3b9f3d2d5bb67cb89783493c8d791bd66a17
TomasBabkineDiCaprio <babkinedicapriotomas@gmail.com>
2021-09-23 19:30:33 -0400

    change to meme[3]

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,7 +13,7 @@ async function displayMeme() {
 
         // required
         console.log(jsonResponse);
-        const meme = jsonResponse.data.memes[1];
+        const meme = jsonResponse.data.memes[3];
 
         const image = document.createElement("img");
         image.src = meme.url;
